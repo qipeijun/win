a0e4a3f5b6794f2f09974526f2f4cbed0df8ce91
戚培俊 <281534481@qq.com>
2019-01-17 20:13:50 +0800

    优化-增加模糊效果

--- js/index.js
+++ js/index.js
@@ -6,7 +6,87 @@ let app = new Vue({
       show:false,
       curImg:1,
       index:0,
-      datas:[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15],
+      datas:[
+        {
+          src:'./img/win/乳胶枕.jpg',
+          srcW:""
+        },
+        {
+          src:'./img/win/天猫精灵.jpg',
+          srcW:""
+        },
+        {
+          src:'./img/win/小米牙刷.jpg',
+          srcW:""
+        },
+        {
+          src:'./img/win/小米耳机.jpg',
+          srcW:""
+        },
+        {
+          src:'./img/win/小米闹钟.jpg',
+          srcW:""
+        },
+        {
+          src:'./img/win/床品四件套.jpg',
+          srcW:""
+        },
+        {
+          src:'./img/win/小熊蒸锅.jpg',
+          srcW:""
+        },
+        {
+          src:'./img/win/小米灯.jpg',
+          srcW:""
+        },
+        {
+          src:'./img/win/帐篷.jpg',
+          srcW:""
+        },
+
+        {
+          src:'./img/win/电磁炉.jpg',
+          srcW:""
+        },
+        {
+          src:'./img/win/登山杖.jpg',
+          srcW:""
+        },
+        {
+          src:'./img/win/空气炸锅.jpg',
+          srcW:""
+        },
+        {
+          src:'./img/win/山地车.jpg',
+          srcW:""
+        },
+        {
+          src:'./img/win/m幸运奖.png',
+          srcW:"./img/win/幸运奖.jpg"
+        },
+        {
+          src:'./img/win/面包机.jpg',
+          srcW:""
+        },
+        {
+          src:'./img/win/m二等奖.png',
+          srcW:"./img/win/二等奖.jpg"
+        },
+        {
+          src:'./img/win/m一等奖.png',
+          srcW:"./img/win/一等奖.jpg"
+        },
+        {
+          src:'./img/win/m三等奖.png',
+          srcW:"./img/win/三等奖.jpg"
+        },
+
+
+
+
+
+
+      ],
       imgs:[],
     }
   },
@@ -14,17 +94,28 @@ let app = new Vue({
     clearDatas(){
       localStorage.removeItem('datas')
     },
-    showImg(n,i){
-      if (n>=1000) {
+    showImg(i,e){
+      let node = e.currentTarget;
+      let src = '';
+      console.log(i);
+      if ($(node).find('img').data('win')) {
+          src = $(node).find('img').data('win')
+      }else {
+        src = $(node).find('img').attr('src')
+      }
+      if (src == './img/click.jpg') {
           return;
       }
       this.show = true;
-      this.curImg = n;
+      this.curImg = src;
       this.index = i;
     },
     hideImg(){
       this.show = false;
-      this.imgs.splice(this.index,1,1000);
+      this.imgs.splice(this.index,1,{
+        src:'./img/click.jpg',
+        srcW:""
+      });
       localStorage.datas = JSON.stringify(this.imgs);
     }
   },
